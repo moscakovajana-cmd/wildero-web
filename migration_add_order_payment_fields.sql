a85022bc04d853866d825bafdf2fcd299d79adfd
moscakovajana-cmd <moscakova.jana@gmail.com>
2026-04-29 21:27:55 +0200

    UI: nahrazeni stareho loga novym s podtitulem

--- migration_add_order_payment_fields.sql
+++
@@ -1,7 +0,0 @@
--- Přidání legislativně povinných sloupců do tabulky objednávek
--- payment_method: způsob platby (dobirka / prevod)
--- shipping_cost: cena dopravy v Kč (pro správné vedení evidence)
-
-ALTER TABLE public.orders
-    ADD COLUMN IF NOT EXISTS payment_method text,
-    ADD COLUMN IF NOT EXISTS shipping_cost integer;
